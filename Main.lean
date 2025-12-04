c2f6e15136335233eed8cccaf2f3a755b01ba070
LQ <234985631+pseudolanguagecompiler@users.noreply.github.com>
2025-12-04 17:36:12 -0500

    Update Main.lean

--- Main.lean
+++ Main.lean
@@ -1,13 +1,20 @@
+import Parser.UniversalParser
 import Ast.UniversalIR
-import Parser.HumanParser
 import Codegen.ToLean
 
 def main (args : List String) : IO UInt32 := do
-  ...
-  match Parser.UniversalParser.parseProgram input with
-  | .error msg => ...
-  | .ok humanAst =>
-      let result ← UniversalIR.evalProgram humanAst (λ_ => none)
-      ...
-      let leanCode ← Codegen.ToLean.fromHuman humanAst
-      ...
+  if args.size != 1 then
+    IO.println "usage: lake exe pseudocode_compiler <file.pseudo>"
+    return 1
+  let input ← IO.FS.readFile args.head!
+  match UniversalParser.parseProgram input with
+  | .error msg => 
+    IO.println s!"Parse error: {msg}"
+    return 1
+  | .ok ast =>
+    let result ← UniversalIR.evalProgram ast (λ_ => none)
+    IO.println s!"Execution result: {result}"
+    let leanCode ← Codegen.ToLean.fromUniversal ast
+    IO.println "\n=== Generated Lean 4 code ==="
+    IO.println leanCode
+    return 0
